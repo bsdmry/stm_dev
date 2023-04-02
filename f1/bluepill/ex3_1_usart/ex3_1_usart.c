5e0e7392a3d7a54e2e035487e1ee4ac02b3a8628
Igor Volodin <postmaster@myghsot.name>
2023-04-03 00:05:09 +0300

    Example 3-1: USART

---
+++ f1/bluepill/ex3_1_usart/ex3_1_usart.c
@@ -0,0 +1,66 @@
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/cm3/dwt.h>
+#include "ex3_1_usart.h"
+
+static void clock_setup(void)
+{
+	rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
+	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_AFIO);
+	rcc_periph_clock_enable(RCC_USART2);
+}
+
+static uint32_t dwt_setup(void)
+{
+        dwt_enable_cycle_counter();
+        __asm volatile ("nop");
+        __asm volatile ("nop");
+        __asm volatile ("nop");
+ 
+     if(dwt_read_cycle_counter())
+     {
+       return 0;
+     }
+     else
+  {
+    return 1;
+  }
+}
+ 
+static void dwt_delay_ms(volatile uint32_t milliseconds)
+{
+        uint32_t initial_ticks = dwt_read_cycle_counter();
+        uint32_t ms_count_tics = milliseconds * (rcc_ahb_frequency / 1000);
+        while ((dwt_read_cycle_counter() - initial_ticks) < ms_count_tics);
+}
+
+static void usart_transmit(char *str)
+{
+	while (*str != '\000') {
+		usart_send_blocking(USART2, *str);
+		str++;
+	}
+}
+
+int main(void)
+{
+	clock_setup();
+	dwt_setup();
+
+ 	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO2 | GPIO3 );
+
+ 	usart_set_baudrate(USART2, 9600);
+ 	usart_set_databits(USART2, 8);
+ 	usart_set_stopbits(USART2, USART_STOPBITS_1);
+ 	usart_set_parity(USART2, USART_PARITY_NONE);
+ 	usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+ 	usart_set_mode(USART2, USART_MODE_TX_RX);
+ 	usart_enable(USART2);
+
+	while(1){
+		dwt_delay_ms(1000);
+  		usart_transmit("Hello from LibOpenCM3!\r\n");
+	}
+}
